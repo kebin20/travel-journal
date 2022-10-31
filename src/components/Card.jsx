667699cc1a6f2e05d8459aa4688edd47406c7be5
kebin20 <ktanzyl@hotmail.com>
2022-10-31 22:32:33 +0900

    Laid out elements using CSS Grid

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,17 @@ import React from "react";
 export default function Card(props) {
     return (
     <div className="card">
-        <img src={props.imageUrl} alt="" />
-        <i className="fa-solid fa-location-dot"></i><span>{props.location}</span><a href={props.googleMapsUrl}>View on Google Maps</a>
-        <h1>{props.title}</h1>
-        <p>{props.startDate}-{props.endDate}</p>
-        <p>{props.description}</p>
+        <img className="card-img"src={props.imageUrl} alt="" />
+        <div className="location">
+            <i className="fa-solid fa-location-dot"></i><span>{props.location}</span><a href={props.googleMapsUrl}>View on Google Maps</a>
+        </div>
+        <div className="titledate">
+            <h1>{props.title}</h1>
+        </div>
+        <div className="description">
+            <p >{props.startDate}-{props.endDate}</p>
+            <p >{props.description}</p>
+        </div>
     </div>
     )
 }
