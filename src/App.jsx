0ddba512a1fa00d55ad540d4b3747dcda5809d6f
kebin20 <ktanzyl@hotmail.com>
2022-11-17 20:08:39 +0900

    INstalled uuid module, added unique id key for each card

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Navbar from "./components/Navbar"
 import Card from "./components/Card"
 import data from "./data"
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
-  const cards = data.map( info => {
+  const cards = data.map(info => {
     return (
       <Card 
-        key={info.id}
+        key={uuidv4()}
         {...info}
       />
     )
